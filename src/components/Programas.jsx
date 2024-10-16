eea2f95297af9bd717e0691ecbd64282833de39c
JhonDaniel01 <jhon-coso@hotmail.com>
2024-10-16 12:08:22 -0500

    Imagenes programas

--- src/components/Programas.jsx
+++ src/components/Programas.jsx
@@ -4,69 +4,69 @@ const Programas = () => {
   return (
     <div className="container mx-auto p-5">
       <h2 className="text-2xl font-bold text-center mb-5">
-        Galería de Imágenes
+        Conoce nuestros programas tecnicos
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {/* Imágenes de los programas */}
         <div className="overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <img
-            src="https://via.placeholder.com/300"
+            src="Transito.jpg"
             alt="Imagen 1"
             className="w-full h-full object-cover"
           />
         </div>
         <div className="overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <img
-            src="https://via.placeholder.com/300"
+            src="Contable.jpg"
             alt="Imagen 2"
             className="w-full h-full object-cover"
           />
         </div>
         <div className="overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <img
-            src="https://via.placeholder.com/300"
+            src="Seguridad.jpg"
             alt="Imagen 3"
             className="w-full h-full object-cover"
           />
         </div>
         <div className="overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <img
-            src="https://via.placeholder.com/300"
+            src="Infancia.jpg"
             alt="Imagen 4"
             className="w-full h-full object-cover"
           />
         </div>
         <div className="overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <img
-            src="https://via.placeholder.com/300"
+            src="Perforador.jpg"
             alt="Imagen 5"
             className="w-full h-full object-cover"
           />
         </div>
         <div className="overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <img
-            src="https://via.placeholder.com/300"
+            src="Transito.jpg"
             alt="Imagen 6"
             className="w-full h-full object-cover"
           />
         </div>
         <div className="overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <img
-            src="https://via.placeholder.com/300"
+            src="Operador.jpg"
             alt="Imagen 7"
             className="w-full h-full object-cover"
           />
         </div>
         <div className="overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <img
-            src="https://via.placeholder.com/300"
+            src="Produccion.jpg"
             alt="Imagen 8"
             className="w-full h-full object-cover"
           />
         </div>
         <div className="overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <img
-            src="https://via.placeholder.com/300"
+            src="Construccion.jpg"
             alt="Imagen 9"
             className="w-full h-full object-cover"
           />
