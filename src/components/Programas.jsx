c887bcc4659da9a1529b3031f6da08df56beda15
JhonDaniel01 <jhon-coso@hotmail.com>
2024-10-21 12:33:49 -0500

    Programas

--- src/components/Programas.jsx
+++ src/components/Programas.jsx
@@ -6,7 +6,7 @@ const Programas = () => {
       <h2 className="text-2xl font-bold text-center mb-5">
         Conoce nuestros programas tecnicos
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {/* Imágenes de los programas */}
         <div className="overflow-hidden rounded-lg shadow-lg transition-transform transform hover:scale-105">
           <img
